547d395ff7ac9421bfc7c66ca510ecbb9c7ae4b7
이종우 <silimy5465@wemade.com>
2024-07-24 18:05:01 +0900

    노트 데이터 구성중

---
+++ Assets/Scripts/Runtime/Data/AudioManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace Runtime.Data
+{
+    public class AudioManager : MonoSingleton<AudioManager>
+    {
+        private AudioSource[] _tickAudioSource;
+
+        private AudioClip _tickAudioClip;
+
+        private const int AudioSourceLength = 10;
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            _tickAudioSource = new AudioSource[AudioSourceLength];
+
+            for (int i = 0; i < AudioSourceLength; i++)
+            {
+                _tickAudioSource[i] = gameObject.AddComponent<AudioSource>();
+            }
+
+            _tickAudioClip ??= Resources.Load<AudioClip>("Audio/assist_tick");
+        }
+
+        public void PlayTick()
+        {
+            for (int i = 0; i < AudioSourceLength; i++)
+            {
+                if (_tickAudioSource[i].isPlaying) continue;
+                
+                _tickAudioSource[i].PlayOneShot(_tickAudioClip);
+            }
+        }
+    }
+}
